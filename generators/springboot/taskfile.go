ed8200eca4d0bdd63942faf589e444dd07580dbf
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2026-01-19 11:06:57 +0530

    feat: Undo Taskfile template processing

--- generators/springboot/taskfile.go
+++ generators/springboot/taskfile.go
@@ -9,8 +9,11 @@ func NewTaskfileConfig(pg projectGenerator) *TaskfileConfig {
 }
 
 func (t TaskfileConfig) generate(pc ProjectConfig) error {
+	//TODO; Taskfile templates has {{ .ArtifactID }} to replace
+	// But it also has many other placeholders using Go templates syntax that shouldn't be processed.
+	// Need to fix it to only replace {{ .ArtifactID }} and ignore other placeholders.
 	if pc.BuildTool == Maven {
-		return t.pg.executeTemplate(pc, "Taskfile.maven.yml.tmpl", "Taskfile.yml")
+		return t.pg.copyTemplateFile(pc, "Taskfile.maven.yml.tmpl", "Taskfile.yml")
 	}
-	return t.pg.executeTemplate(pc, "Taskfile.gradle.yml.tmpl", "Taskfile.yml")
+	return t.pg.copyTemplateFile(pc, "Taskfile.gradle.yml.tmpl", "Taskfile.yml")
 }
